e2dc4e0967880f10b5d55d1de84de530c8b7d30e
SistemasCAE <sistemas_cae@cae3076.com>
2017-06-23 15:18:45 -0500

    Add files via upload

--- js/almacena.js
+++ js/almacena.js
@@ -60,7 +60,7 @@ var almacena = {
 					est = "&nbsp;"
 				}
 				est='No Enviado';
-				resultado += "<tr><td>"+(i+1).toString()+"</td><td>"+usu+"</td><td><a href='#' class='folio' urlImagen='"+img+"'>"+inf+"</a></td><td><img src='"+est+"'></td></tr>";
+				resultado += "<tr><td>"+(i+1).toString()+"</td><td>"+usu+"</td><td><a href='#' class='folio' urlImagen='"+img+"'>"+inf+"</a></td><td>'"+est+"'</td></tr>";
 			}
 		}
 		//$("#informacion").removeClass("ui-table");
